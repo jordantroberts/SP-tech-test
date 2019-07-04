223b1e6245f94f4e72a68f2cceedb1a2659eaa66
Jordan Roberts <jordanroberts@talk21.com>
2019-07-04 09:43:31 +0100

    I added a testserver.log with smaller amount of test files.
    I added sort_by_page_view method which shows how many times a page url has been visited

--- lib/parse.rb
+++ lib/parse.rb
@@ -1,11 +1,28 @@
 class Parse
 
+  attr_reader :webpages, :url_array, :page_views
+
+  def initialize
+    @webpages = []
+    @url_array = []
+    @page_views = Hash.new(0)
+
+  end
+
   def parser(file)
     file_contents = ""
     File.foreach(file) do |line|
+      @webpages << line
       file_contents += line
     end
     return file_contents
   end
 
+  def sort_by_page_view
+    new_array = @webpages.map { |i| i.split(' ') }
+    @urlarray = new_array.map { |item| item[0] }
+    @urlarray.each { |url| @page_views[url] += 1 }
+    ordered = @page_views.sort_by { |key, value| value }.reverse
+    return ordered
+  end
 end
--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -3,8 +3,15 @@ require 'parse'
 describe Parse do
 
   describe '#parser' do
-    it 'prints contents of log file' do
-      expect(subject.parser('./webserver.log')).to eq File.read('./webserver.log')
+    it 'parses contents of log file' do
+      expect(subject.parser('./testserver.log')).to eq File.read('./testserver.log')
+    end
+  end
+
+  describe 'sort_by_page_view' do
+    it 'sorts webpages into order of page views' do
+      subject.parser('./testserver.log')
+      expect(subject.sort_by_page_view).to eq([["/home", 3], ["/about/2", 2], ["/about", 1], ["/contact", 1]])
     end
   end
 
