0849dba526945ab3243124b3b2f539dc2799e57a
Jordan Roberts <jordanroberts@talk21.com>
2019-07-04 18:52:18 +0100

    Added a List class which format array and output a neater list telling the user how many times a page was visited

---
+++ lib/list.rb
@@ -0,0 +1,13 @@
+require_relative 'parse'
+
+class List
+
+  def list_views(array)
+    array.map { |url, views| puts url + " was visited #{views} times" }
+  end
+
+  def list_unique_views(array)
+    array.map { |url, views| puts url + " was visited #{views} unique times" }
+  end
+
+end
--- lib/parse.rb
+++ lib/parse.rb
@@ -1,13 +1,16 @@
+require_relative 'list'
+
 class Parse
 
-  attr_reader :webpages, :url_array, :page_views
+  attr_reader :webpages, :url_array, :page_views, :ordered_by_ips, :ordered_by_views
 
   def initialize
     @webpages = []
     @url_array = []
     @page_views = Hash.new(0)
     @unique_views = Hash.new(0)
-
+    @ordered_by_ips = []
+    @ordered_by_views = []
   end
 
   def parser(file)
@@ -23,8 +26,8 @@ class Parse
     new_array = @webpages.map { |i| i.split(' ') }
     @url_array = new_array.map { |item| item[0] }
     @url_array.each { |url| @page_views[url] += 1 }
-    ordered = @page_views.sort_by { |key, value| value }.reverse
-    return ordered
+    @ordered_by_views = @page_views.sort_by { |key, value| value }.reverse
+    return @ordered_by_views
   end
 
   def sort_by_unique_view
@@ -32,7 +35,17 @@ class Parse
     unique = new_array.uniq
     unique = unique.map { |item| item[0] }
     unique.each { |url| @unique_views[url] += 1 }
-    ordered_ips = @unique_views.sort_by { |key, value| value }.reverse
-    return ordered_ips
+    @ordered_by_ips = @unique_views.sort_by { |key, value| value }.reverse
+    return @ordered_by_ips
+  end
+
+  def see_views
+    list = List.new
+    list.list_views(@ordered_by_views)
+  end
+
+  def see_unique_views
+    list = List.new
+    list.list_unique_views(@ordered_by_ips)
   end
 end
